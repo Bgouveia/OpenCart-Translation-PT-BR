828631ba1fdc210d145223f8f08fb2449eec5355
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-06 19:22:35 -0300

    traduzindo vouchers

---
+++ admin/language/portuguese-br/sale/voucher.php
@@ -0,0 +1,43 @@
+<?php
+// Heading  
+$_['heading_title']     = 'Voucher para presente';
+
+// Text
+$_['text_send']         = 'Enviar';
+$_['text_success']      = 'Sucesso: Você alterou os vouchers!';
+$_['text_sent']         = 'O voucher de presente vou enviado por e-mail!';
+$_['text_wait']         = 'Por favor, aguarde!';
+
+// Column
+$_['column_name']       = 'Nome do voucher';
+$_['column_code']       = 'Código';
+$_['column_from']       = 'De';
+$_['column_to']         = 'Para';
+$_['column_amount']     = 'Quantidade';
+$_['column_theme']      = 'Tema';
+$_['column_date_added'] = 'Data de criação';
+$_['column_status']     = 'Situação';
+$_['column_order_id']   = 'ID do pedido';
+$_['column_customer']   = 'Cliente';
+$_['column_date_added'] = 'Data de criação';
+$_['column_action']     = 'Ação';
+
+// Entry
+$_['entry_code']        = 'Código:<br /><span class="help">O código para o cliente ativar o voucher.</span>';
+$_['entry_from_name']   = 'Remetente:';
+$_['entry_from_email']  = 'E-Mail do remetente:';
+$_['entry_to_name']     = 'Destinatário:';
+$_['entry_to_email']    = 'E-Mail do destinatário:';
+$_['entry_message']     = 'Mensagem:';
+$_['entry_amount']      = 'Quantidade:';
+$_['entry_theme']       = 'Tema:';
+$_['entry_status']      = 'Situação:';
+
+// Error
+$_['error_permission']  = 'Atenção: Você não possui permissão para alterar vouchers!';
+$_['error_code']        = 'O código deve ter entre 3 e 10 caracteres!';
+$_['error_to_name']     = 'O nome do destinatário deve ter entre 1 e 64 caracteres!';
+$_['error_from_name']   = 'Seu nome deve estar entre 1 e 64 caracteres!';
+$_['error_email']       = 'O endereço do e-mail não é válido!';
+$_['error_amount']      = 'A quantidade de vouchers deve ser maior ou igual a 1!';
+?>
---
+++ admin/language/portuguese-br/sale/voucher_theme.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'Temas de Vouchers';
+
+// Text
+$_['text_success']       = 'Sucesso: Você alterou os temas de voucher!';
+$_['text_image_manager'] = 'Gerenciador de imagens';
+
+// Column
+$_['column_name']        = 'Nome do tema';
+$_['column_action']      = 'Ação';
+
+// Entry
+$_['entry_name']         = 'Nome do tema:';
+$_['entry_description']  = 'Descrição do tema:';
+$_['entry_image']        = 'Imagem:';
+
+// Error
+$_['error_permission']  = 'Atenção: Você não possui permissão para alterar temas de voucher!';
+$_['error_name']        = 'Nomes de temas para vouchers devem ter entre 3 e 32 caracteres!';
+$_['error_image']       = 'Imagem obrigatória!';
+$_['error_voucher']     = 'Atenção: este tema para vouchers não pode ser removido pois está atrelado a %s vouchers!';
+?>
