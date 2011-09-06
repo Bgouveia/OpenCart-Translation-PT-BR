4a164768c782fc98c64f8ea4f8457a1e3d82e444
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-06 18:10:00 -0300

    traduzindo designs

---
+++ admin/language/portuguese-br/design/banner.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']      = 'Banners';
+
+// Text
+$_['text_success']       = 'Sucesso: Você alterou os banners!';
+$_['text_default']       = 'Padrão';
+$_['text_image_manager'] = 'Gerenciador de imagens';
+
+// Column
+$_['column_name']        = 'Nome do banner';
+$_['column_status']      = 'Situação';
+$_['column_action']      = 'Ação';
+
+// Entry
+$_['entry_name']         = 'Nome do banner:';
+$_['entry_title']        = 'Título:';
+$_['entry_link']         = 'Link:';
+$_['entry_image']        = 'Imagem:';
+$_['entry_status']       = 'Situação:';
+
+// Error
+$_['error_permission']   = 'Atenção: Você não possui permissão para alterar banners!';
+$_['error_name']         = 'O nome do banner deve ter entre 3 e 64 caracteres!';
+$_['error_title']        = 'O título do banner deve ter entre 2 e 64 caracteres!';
+$_['error_default']      = 'Atenção: Este layout não pode ser apagado pois está definido como o layout de loja padrão!';
+$_['error_product']      = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s produtos!';
+$_['error_category']     = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s categorias!';
+$_['error_information']  = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s páginas de informação!';
+?>
---
+++ admin/language/portuguese-br/design/layout.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']     = 'Layouts';
+
+// Text
+$_['text_success']      = 'Success: You have modified layouts!';
+$_['text_default']      = 'Default';
+
+// Column
+$_['column_name']       = 'Layout Name';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_name']        = 'Layout Name:';
+$_['entry_store']       = 'Store:';
+$_['entry_route']       = 'Route:';
+
+// Error
+$_['error_permission']  = 'Atenção: Você não possui permissão para alterar layouts!';
+$_['error_name']        = 'Layout Name deve ter entre 3 e 64 caracteres!';
+$_['error_default']      = 'Atenção: Este layout não pode ser apagado pois está definido como o layout de loja padrão!';
+$_['error_store']       = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s loja(s)!';
+$_['error_product']      = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s produtos!';
+$_['error_category']     = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s categorias!';
+$_['error_information']  = 'Atenção: Este layout não pode ser apagado pois está atrelado a %s páginas de informação!';
+?>
