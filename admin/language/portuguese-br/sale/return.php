8a40e8e22806577eff3b71b0a54208890cb6be2d
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-06 19:22:28 -0300

    traduzindo trocas

---
+++ admin/language/portuguese-br/sale/return.php
@@ -0,0 +1,71 @@
+<?php
+// Heading
+$_['heading_title']       = 'Troca de produtos';
+
+// Text
+$_['text_opened']         = 'Aberta';
+$_['text_unopened']       = 'Fechada';
+$_['text_success']        = 'Sucesso: Você alterou as trocas!';
+$_['text_wait']           = 'Por favor, aguarde!';
+
+// Text
+$_['text_return_id']      = 'ID da troca:';
+$_['text_order_id']       = 'ID do pedido:';
+$_['text_date_ordered']   = 'Data do pedido:';
+$_['text_customer']       = 'Cliente:';
+$_['text_email']          = 'E-Mail:';
+$_['text_telephone']      = 'Telefone:';
+$_['text_return_status']  = 'Situação da troca:';
+$_['text_comment']        = 'Comentário:';
+$_['text_date_added']     = 'Data da solicitação:';
+$_['text_date_modified']  = 'Data de alteração:';
+
+// Column
+$_['column_return_id']     = 'ID da troca';
+$_['column_order_id']      = 'ID do pedido';
+$_['column_customer']      = 'Cliente';
+$_['column_quantity']      = 'Quantidade';
+$_['column_status']        = 'Situação';
+$_['column_date_added']    = 'Data da solicitação';
+$_['column_date_modified'] = 'Data de alteração';
+$_['column_product']       = 'Produto';
+$_['column_model']         = 'Modelo';
+$_['column_reason']        = 'Motivo da troca';
+$_['column_opened']        = 'Aberta';
+$_['column_notify']        = 'Notificar cliente';
+$_['column_comment']       = 'Comentário';
+$_['column_action']        = 'Ação';
+
+// Entry
+$_['entry_customer']      = 'Cliente:';
+$_['entry_order_id']      = 'ID do pedido:';
+$_['entry_date_ordered']  = 'Data do pedido:';
+$_['entry_firstname']     = 'Nome:';
+$_['entry_lastname']      = 'Sobrenome:';
+$_['entry_email']         = 'E-Mail:';
+$_['entry_telephone']     = 'Telefone:';
+$_['entry_product']       = 'Produto:';
+$_['entry_model']         = 'Modelo:';
+$_['entry_quantity']      = 'Quantidade:';
+$_['entry_reason']        = 'Motivo da troca:';
+$_['entry_opened']        = 'Aberta:';
+$_['entry_comment']       = 'Comentário:';
+$_['entry_return_status'] = 'Situação da troca:';
+$_['entry_notify']        = 'Notificar cliente:';
+$_['entry_action']        = 'Ação da troca:';
+
+// Error
+$_['error_warning']       = 'Atenção: Por favor, verifique os erros no formulário!';
+$_['error_permission']    = 'Atenção: Você não possui permissão para alterar trocas!';
+$_['error_firstname']     = 'O nome deve ter entre 1 e 32 caracteres!';
+$_['error_lastname']      = 'Sobrenome deve ter entre 1 e 32 caracteres!';
+$_['error_email']         = 'O endereço de e-mail está inválido!';
+$_['error_telephone']     = 'O Telefone deve ter entre 3 e 32 caracteres!';
+$_['error_password']      = 'A senha deve ter entre 3 e 20 caracteres!';
+$_['error_confirm']       = 'A senha e a confirmação de senha não são iguais!';
+$_['error_address_1']     = 'O primeiro endereço deve ter entre 3 e 128 caracteres!';
+$_['error_city']          = 'A cidade deve ter entre 3 e 128 caracteres!';
+$_['error_postcode']      = 'O código postal deve ter entre 2 e 10 caracteres!';
+$_['error_country']       = 'Favor selecionar uma país!';
+$_['error_zone']          = 'Favor selecionar um estado!';
+?>
