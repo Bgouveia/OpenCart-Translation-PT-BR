b742bf10a431630e0b37132a8bc18b72350b26af
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-06 19:22:15 -0300

    traduzindo afiliados

---
+++ admin/language/portuguese-br/sale/affiliate.php
@@ -0,0 +1,70 @@
+<?php
+// Heading
+$_['heading_title']             = 'Afialiados';
+
+// Text
+$_['text_success']              = 'Sucesso: Você alterou os afiliados!';
+$_['text_approved']             = 'Você aprovou %s conta(s)!';
+$_['text_wait']                 = 'Por favor, aguarde!';
+$_['text_balance']              = 'Saldo:';
+$_['text_cheque']               = 'Cheque';
+$_['text_paypal']               = 'PayPal';
+$_['text_bank']                 = 'Transferência bancária';
+
+// Column
+$_['column_name']               = 'Nome do afialiado';
+$_['column_email']              = 'E-Mail';
+$_['column_code']               = 'Código de rastreamento';
+$_['column_balance']            = 'Saldo';
+$_['column_status']             = 'Situação';
+$_['column_approved']           = 'Aprovado';
+$_['column_date_added']         = 'Data de início';
+$_['column_description']        = 'Descrição';
+$_['column_amount']             = 'Quantidade';
+$_['column_action']             = 'Ação';
+
+// Entry
+$_['entry_firstname']           = 'Nome:';
+$_['entry_lastname']            = 'Sobrenome:';
+$_['entry_email']               = 'E-Mail:';
+$_['entry_telephone']           = 'Telefone:';
+$_['entry_fax']                 = 'Fax:';
+$_['entry_status']              = 'Situação:';
+$_['entry_password']            = 'Senha:';
+$_['entry_confirm']             = 'Confirme:';
+$_['entry_company']             = 'Empresa:';
+$_['entry_address_1']           = 'Endereço 1:';
+$_['entry_address_2']           = 'Endereço 2:';
+$_['entry_city']                = 'Cidade:';
+$_['entry_postcode']            = 'Código postal:';
+$_['entry_country']             = 'País:';
+$_['entry_zone']                = 'Estado:';
+$_['entry_code']                = 'Código de rastreamento:<span class="help">O código de rastreamento que será usado para monitoração do afiliado.</span>';
+$_['entry_commission']          = 'Comissão (%):<span class="help">Percentual recebido pelos afialiados, entre si.</span>';
+$_['entry_tax']                 = 'ID da taxa:';
+$_['entry_payment']             = 'Método de pagamento:';
+$_['entry_cheque']              = 'Nome do favorecido:';
+$_['entry_paypal']              = 'E-mail da conta do PayPal:';
+$_['entry_bank_name']           = 'Nome do banco:';
+$_['entry_bank_branch_number']  = 'Número do banco:';
+$_['entry_bank_swift_code']     = 'Código do banco (SWIFT Code):';
+$_['entry_bank_account_name']   = 'Nome do titular da conta:';
+$_['entry_bank_account_number'] = 'Número da conta:';
+$_['entry_amount']              = 'Quantidade:';
+$_['entry_description']         = 'Descrição:';
+
+// Error
+$_['error_permission']          = 'Atenção: Você não possui permissão para alterar afiliados!';
+$_['error_firstname']     = 'O nome deve ter entre 1 e 32 caracteres!';
+$_['error_lastname']      = 'Sobrenome deve ter entre 1 e 32 caracteres!';
+$_['error_email']         = 'O endereço de e-mail está inválido!';
+$_['error_telephone']     = 'O Telefone deve ter entre 3 e 32 caracteres!';
+$_['error_password']      = 'A senha deve ter entre 3 e 20 caracteres!';
+$_['error_confirm']       = 'A senha e a confirmação de senha não são iguais!';
+$_['error_address_1']     = 'O primeiro endereço deve ter entre 3 e 128 caracteres!';
+$_['error_city']          = 'A cidade deve ter entre 3 e 128 caracteres!';
+$_['error_postcode']      = 'O código postal deve ter entre 2 e 10 caracteres!';
+$_['error_country']       = 'Favor selecionar uma país!';
+$_['error_zone']          = 'Favor selecionar um estado!';
+$_['error_code']          = 'O código de rastreamento é obrigatório!';
+?>
