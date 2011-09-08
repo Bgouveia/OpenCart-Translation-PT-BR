393bca4ff23e1b746cb54b3f52b675571525f755
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-08 11:43:24 -0300

    traduzindo trocas (catalogo)

---
+++ catalog/language/portuguese-br/account/return.php
@@ -0,0 +1,63 @@
+<?php
+// Heading 
+$_['heading_title']      = 'Troca de produtos';
+
+// Text
+$_['text_account']       = 'Conta';
+$_['text_return']        = 'Informações sobre a troca';
+$_['text_description']   = '<p>Por favor, preencha o formulário a seguir para receber um numero de protocolo de troca.</p>';
+$_['text_order']         = 'Informações do pedido';
+$_['text_product']       = 'Informações do produto &amp; Motivo para a troca';
+$_['text_additional']    = 'Informações adicionais';
+$_['text_message']       = '<p>Obrigado por submeter sua solicitação para troca. Sua solicitação foi enviada para o departamento responsável.</p><p> Você será notificado por e-mail sobre a situação da sua solicitação.</p>';
+$_['text_return_id']     = 'ID da troca:';
+$_['text_order_id']      = 'ID do pedido:';
+$_['text_date_ordered']  = 'Data do pedido:';
+$_['text_status']        = 'Situação:';
+$_['text_date_added']    = 'Data de adição:';
+$_['text_customer']      = 'Cliente:';
+$_['text_comment']       = 'Comentários sobre a troca';
+$_['text_products']      = 'Produtos:';
+$_['text_history']       = 'Histórico da troca';
+$_['text_empty']         = 'Não existe um histórico de trocas!';
+$_['text_error']         = 'A troca selecionada não pode ser encontrada!';
+
+// Column
+$_['column_name']        = 'Nome do produto';
+$_['column_model']       = 'Modelo';
+$_['column_quantity']    = 'Quantidade';
+$_['column_price']       = 'Valor';
+$_['column_opened']      = 'Abertura';
+$_['column_comment']     = 'Comentário';
+$_['column_reason']      = 'Motivo';
+$_['column_action']      = 'Ação';
+$_['column_date_added']  = 'Data de adição';
+$_['column_status']      = 'Situação';
+
+// Entry
+$_['entry_order_id']     = 'ID do pedido:';
+$_['entry_date_ordered'] = 'Data do pedido:';
+$_['entry_firstname']    = 'Nome:';
+$_['entry_lastname']     = 'Sobrenome:';
+$_['entry_email']        = 'E-Mail:';
+$_['entry_telephone']    = 'Telefone:';
+$_['entry_product']      = 'Nome do produto:';
+$_['entry_model']        = 'Código do produto:';
+$_['entry_quantity']     = 'Quantidade:';
+$_['entry_reason']       = 'Motivo para troca:';
+$_['entry_opened']       = 'Produto está aberto:';
+$_['entry_fault_detail'] = 'Defeitos ou outros detalhes:';
+$_['entry_captcha']      = 'Digite o código na caixa abaixo:';
+
+// Error
+$_['error_order_id']     = 'ID do pedido é obrigatório!';
+$_['error_firstname']    = 'O campo nome deve ter entre 1 e 32 caracteres!';
+$_['error_lastname']     = 'O campo sobrenome deve ter entre 1 e 32 caracteres!';
+$_['error_email']        = 'O endereço do e-mail parece estar inválido!';
+$_['error_telephone']    = 'O telefone deve ter entre 3 e 32 caracteres!';
+$_['error_product']      = 'Deve ser selecionado no mínimo um produto!';
+$_['error_name']         = 'O nome do produto deve ter entre 3 e 255 caracteres!';
+$_['error_model']        = 'O modelo do produto deve ter entre 3 e 64 caracteres!';
+$_['error_reason']       = 'Você deve selecionar um motivo para a troca!';
+$_['error_captcha']      = 'O código de verificação digitado não bate com a imagem exibida!';
+?>
