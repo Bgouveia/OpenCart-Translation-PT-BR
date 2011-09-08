ec80a2b9ffdac101a92d790664759024d2148ebf
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-08 11:43:12 -0300

    traduzindo registro de conta

---
+++ catalog/language/portuguese-br/account/register.php
@@ -0,0 +1,46 @@
+<?php
+// Heading 
+$_['heading_title']        = 'Criar conta';
+
+// Text
+$_['text_account']         = 'Conta';
+$_['text_register']        = 'Criar';
+$_['text_account_already'] = 'Se você já possui uma conta, por favor <a href="%s">faça o login</a>.';
+$_['text_your_details']    = 'Informações pessoais';
+$_['text_your_address']    = 'Seu endereço';
+$_['text_newsletter']      = 'Notícias';
+$_['text_your_password']   = 'Sua senha';
+$_['text_agree']           = 'Eu li e aceito os <a class="fancybox" href="%s" alt="%s"><b>%s</b></a>';
+
+// Entry
+$_['entry_firstname']      = 'Nome:';
+$_['entry_lastname']       = 'Sobrenome:';
+$_['entry_email']          = 'E-Mail:';
+$_['entry_telephone']      = 'Telefone:';
+$_['entry_fax']            = 'Fax:';
+$_['entry_company']        = 'Empresa:';
+$_['entry_address_1']      = 'Endereço 1:';
+$_['entry_address_2']      = 'Endereço 2:';
+$_['entry_postcode']       = 'Código postal:';
+$_['entry_city']           = 'Cidade:';
+$_['entry_country']        = 'País:';
+$_['entry_zone']           = 'Estado:';
+$_['entry_newsletter']     = 'Inscrever-se:';
+$_['entry_password']       = 'Senha:';
+$_['entry_confirm']        = 'Confirme a senha:';
+
+// Error
+$_['error_exists']         = 'Atenção: Este endereço de e-mail já está registrado!';
+$_['error_firstname']      = 'O nome deve ter entre 1 e 32 caracteres!';
+$_['error_lastname']       = 'O sobrenome deve ter entre 1 e 32 caracteres!';
+$_['error_email']          = 'O endereço de e-mail parece ser inválido!';
+$_['error_telephone']      = 'O telefone deve ter entre 3 e 32 caracteres!';
+$_['error_password']       = 'Sua senha deve ter entre 4 e 20 caracteres!';
+$_['error_confirm']        = 'A senha e a confirmação da senha não são idênticas';
+$_['error_address_1']      = 'O endereço 1 deve ter entre 3 e 128 caracteres!';
+$_['error_city']           = 'O nome da cidade deve ter entre 2 e 128 caracteres!';
+$_['error_postcode']       = 'O código postal deve ter entre 2 e 10 caracteres!';
+$_['error_country']        = 'Por favor, selecione um país!';
+$_['error_zone']           = 'Por favor, selecione um estado!';
+$_['error_agree']          = 'Atenção: Para concluir, você deve aceitar os %s!';
+?>
