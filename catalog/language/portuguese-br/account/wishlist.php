f7537f7074bd30e5b3e684647daf317b844c973a
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-08 11:10:20 -0300

    traduzindo lista de desejos

---
+++ catalog/language/portuguese-br/account/wishlist.php
@@ -0,0 +1,21 @@
+<?php
+// Heading 
+$_['heading_title'] = 'Lista de desejos';
+
+// Text
+$_['text_account']  = 'Conta';
+$_['text_instock']  = 'Em estoque';
+$_['text_success']  = 'Você adicionou <a href="%s">%s</a> a sua <a href="%s">lista de desejos</a> com sucesso!';
+$_['text_wishlist'] = 'Lista de desejos (%s)';
+$_['text_login']    = 'Você deve efetuar o <a href="%s">login</a> ou <a href="%s">criar uma conta</a> para salvar <a href="%s">%s</a> na sua <a href="%s">lista de desejos</a>!';
+$_['text_empty']    = 'Sua lista de desejos está vazia.';
+
+// Column
+$_['column_remove'] = 'Remover';
+$_['column_image']  = 'Imagem';
+$_['column_name']   = 'Nome do produto';
+$_['column_model']  = 'Modelo';
+$_['column_stock']  = 'Estoque';
+$_['column_price']  = 'Preço unitário';
+$_['column_cart']   = 'Comprar';
+?>
