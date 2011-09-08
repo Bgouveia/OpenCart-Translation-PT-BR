b5d2c50c6b4e8240e25bc7192af58fbb42bcd57e
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-08 11:10:09 -0300

    traduzindo transações

---
+++ catalog/language/portuguese-br/account/transaction.php
@@ -0,0 +1,15 @@
+<?php
+// Heading 
+$_['heading_title']      = 'Suas transações';
+
+// Column
+$_['column_date_added']  = 'Data de adição';
+$_['column_description'] = 'Descrição';
+$_['column_amount']      = 'Quantidade (%s)';
+
+// Text
+$_['text_account']       = 'Conta';
+$_['text_transaction']   = 'Suas transações';
+$_['text_total']         = 'Seu saldo atual é:';
+$_['text_empty']         = 'Você não possui transações!';
+?>
