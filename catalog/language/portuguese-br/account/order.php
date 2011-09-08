ee3d999f564966fd34498a9f15b183516af941b5
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-08 10:58:05 -0300

    traduzindo pedidos do catalogo

---
+++ catalog/language/portuguese-br/account/order.php
@@ -0,0 +1,41 @@
+<?php
+// Heading 
+$_['heading_title']         = 'Histórico de pedidos';
+
+// Text
+$_['text_account']          = 'Conta';
+$_['text_order']            = 'Informações do pedido';
+$_['text_order_detail']     = 'Detalhes do pedido';
+$_['text_invoice_no']       = 'Número da fatura.:';
+$_['text_order_id']         = 'ID do pedido:';
+$_['text_status']           = 'Situação:';
+$_['text_date_added']       = 'Data inicial:';
+$_['text_customer']         = 'Cliente:';
+$_['text_shipping_address'] = 'Endereço para envio';
+$_['text_shipping_method']  = 'Método de envio:';
+$_['text_payment_address']  = 'Endereço de pagamento';
+$_['text_payment_method']   = 'Método de pagamento:';
+$_['text_products']         = 'Produtos:';
+$_['text_total']            = 'Total:';
+$_['text_comment']          = 'Cometários do pedido';
+$_['text_history']          = 'Histórico do pedido';
+$_['text_empty']            = 'Você não efetuou pedidos anteriores!';
+$_['text_error']            = 'O pedido solicitado não foi encontrado!';
+$_['text_action']           = 'Escolha uma ação:';
+$_['text_selected']         = 'Selecionados..';
+$_['text_reorder']          = 'Adicionar ao carrinho';
+$_['text_return']           = 'Voltar para "Produtos"';
+
+// Column
+$_['column_name']           = 'Nome do produto';
+$_['column_model']          = 'Modelo';
+$_['column_quantity']       = 'Quantidade';
+$_['column_price']          = 'Valor';
+$_['column_total']          = 'Total';
+$_['column_date_added']     = 'Data de adição';
+$_['column_status']         = 'Situação';
+$_['column_comment']        = 'Comentário';
+
+// Error
+$_['error_warning']         = 'Atenção: Você deve selecionar ao menos um produto e escolher uma ação para prosseguir!';
+?>
