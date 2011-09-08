934a0908450be29f1b5aa38cbf185dd3c0bfec4d
Bruno Gouveia <brunoillusions@gmail.com>
2011-09-08 11:43:43 -0300

    traduzindo recompensas (pontos - catalogo)

---
+++ catalog/language/portuguese-br/account/reward.php
@@ -0,0 +1,15 @@
+<?php
+// Heading 
+$_['heading_title']      = 'Seus pontos';
+
+// Column
+$_['column_date_added']  = 'Data de adição';
+$_['column_description'] = 'Descrição';
+$_['column_points']      = 'Pontos';
+
+// Text
+$_['text_account']       = 'Conta';
+$_['text_reward']        = 'Pontos';
+$_['text_total']         = 'A quantidade de pontos que você possui é:';
+$_['text_empty']         = 'Você não possui pontos!';
+?>
